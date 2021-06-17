a35da76669414a3d6cb6a2da2e2d81254f599c06
Ben Shutt <ben@3sidedcube.com>
2021-06-17 11:53:53 +0100

    SwiftLint autocorrect and update default test

--- Tests/AttributedStringTests/AttributedStringTests.swift
+++ Tests/AttributedStringTests/AttributedStringTests.swift
@@ -1,11 +1,9 @@
-    import XCTest
-    @testable import AttributedString
+import XCTest
+@testable import AttributedString
 
-    final class AttributedStringTests: XCTestCase {
-        func testExample() {
-            // This is an example of a functional test case.
-            // Use XCTAssert and related functions to verify your tests produce the correct
-            // results.
-            XCTAssertEqual(AttributedString().text, "Hello, World!")
-        }
+final class AttributedStringTests: XCTestCase {
+
+    func testExample() {
+        XCTAssertTrue(true)
     }
+}
